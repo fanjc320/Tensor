86c493508bdc0444c09653c3ba4aef8233f378a9
fanjinchang <894020798@qq.com>
2018-08-26 15:24:11 +0800

    test

--- PyAudio/guolv.py
+++ PyAudio/guolv.py
@@ -3,7 +3,7 @@ from scipy.fftpack import fft,ifft
 import matplotlib.pyplot as plt
 import seaborn
 
-
+plt.tight_layout()
 #采样点选择1400个，因为设置的信号频率分量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，所以这里设置采样频率为1400赫兹（即一秒内有1400个采样点，一样意思的）
 x=np.linspace(0,1,1400)      
 
@@ -15,14 +15,27 @@ yreal = yy.real               # 获取实数部分
 yimag = yy.imag               # 获取虚数部分
 
 
-plt.subplot(221)
+plt.subplot(421)
 plt.plot(x[0:50],y[0:50])   
 plt.title('Original wave')
 
 xf = np.arange(len(y))        # 频率
-plt.subplot(222)
+plt.subplot(422)
 plt.plot(xf,yy,'r')
 plt.title('FFT of Mixed wave(two sides frequency range)',fontsize=7,color='#7A378B')  #注意这里的颜色可以查询颜色代码表
+ 
+plt.subplot(423)
+plt.plot(xf[:100],yy[:100],'b')
+plt.subplot(424)
+plt.plot(xf[:500],yy[:500],'b')
+
+yy_ = ifft(yy)
+plt.subplot(425)
+plt.plot(xf[:50],yy_[:50],'b')
+
+yy_ = ifft(yy[:500])
+plt.subplot(426)
+plt.plot(xf[:50],yy_[:50],'g')
 
 
 plt.show()
