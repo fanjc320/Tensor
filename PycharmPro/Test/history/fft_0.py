207fd72ebbd53c8031281cd6b1825dae006d97f2
fanjinchang <894020798@qq.com>
2018-09-13 00:03:41 +0800

    hhhhh

--- PycharmPro/Test/history/fft_0.py
+++ PycharmPro/Test/history/fft_0.py
@@ -12,38 +12,24 @@ y=7*np.sin(2*np.pi*10*x) + 3*np.sin(2*np.pi*30*x)+5*np.sin(2*np.pi*60*x)
 print("sum(y)",sum(abs(y)),sum(y*y))
 
 yy=fft(y)                     #快速傅里叶变换
-yreal = yy.real               # 获取实数部分
-yimag = yy.imag               # 获取虚数部分
+# yreal = yy.real               # 获取实数部分
+# yimag = yy.imag               # 获取虚数部分
 
 yf=abs(fft(y))                # 取绝对值
 print("sum(yf)",sum(yf),sum(yf*yf))
-yf1=abs(fft(y))/len(x)           #归一化处理
-yf2 = yf1[range(int(len(x)/2))]  #由于对称性，只取一半区间
-yf3 = yf1[range(int(len(x)))]
+yf3 = yf[range(int(len(x)))]
 
 xf = np.arange(len(y))        # 频率
 xf1 = xf
 xf2 = xf[range(int(len(x)/2))]  #取一半区间
 xf3 = xf[range(int(len(x)))]
 
-plt.subplot(321)
-plt.plot(x,y)
+plt.subplot(311)
+plt.stem(x,y)
 plt.title('Original wave')
 
-plt.subplot(322)
-plt.plot(xf,yf,'r')
-plt.title('FFT of Mixed wave(two sides frequency range)',fontsize=7,color='#7A378B')  #注意这里的颜色可以查询颜色代码表
-
-plt.subplot(323)
-plt.plot(xf1,yf1,'g')
-plt.title('FFT of Mixed wave(normalization)',fontsize=9,color='r')
-
-plt.subplot(324)
-plt.plot(xf2,yf2,'b')
-plt.title('FFT of Mixed wave)',fontsize=10,color='#F08080')
-
-plt.subplot(325)
-plt.plot(xf3,yf3,'b')
+plt.subplot(312)
+plt.stem(xf3,yf3,'b')
 plt.title('FFT of Mixed wave*2)',fontsize=10,color='#F08080')
 
 print(" len(x)",len(x)," len(y) ",len(y),"xf2",xf2);
