27469671964db7ec73186268b7a0d92be2c67bc2
fanjinchang <894020798@qq.com>
2019-02-13 11:08:47 +0800

    dddddddddd

--- LoggerTool_py/ToMysql.py
+++ LoggerTool_py/ToMysql.py
@@ -77,7 +77,7 @@ import datetime
 # G_TB_List = []
 
 def get_conn():
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='tlogt', charset='utf8')
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='tlog_t', charset='utf8')
     return conn
 
 def createTb(conn,G_TB_List,withhead=0,drop = 1):
