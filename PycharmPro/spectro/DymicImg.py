52551df8f318ac85902d903cbc32b910940b4249
fanjinchang <894020798@qq.com>
2019-04-09 15:16:10 +0800

    kkkkk

--- PycharmPro/spectro/DymicImg.py
+++ PycharmPro/spectro/DymicImg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+from matplotlib.animation import FuncAnimation
+from matplotlib import animation
 
 plt.axis([0,100,0,1])
 plt.ion()
@@ -28,4 +31,49 @@ def DyImg1():
         plt.pause(0.1)
 
 # DyImg0()
-DyImg1()
+# DyImg1()
+
+
+fig, ax = plt.subplots()
+xdata, ydata = [], []
+ln, = plt.plot([], [], 'ro')
+
+def init():
+    print("init-----")
+    ax.set_xlim(0, 2*np.pi)
+    ax.set_ylim(-1, 1)
+    return ln,
+
+def update(frame):
+    print("updata-----")
+    xdata.append(frame)
+    ydata.append(np.sin(frame))
+    ln.set_data(xdata, ydata)
+    return ln,
+
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
+                    init_func=init, blit=True)
+plt.show()
+
+input()
+
+# def update(i):
+#     label = 'timestep {0}'.format(i)
+#     print(label)
+#     # 更新直线和x轴（用一个新的x轴的标签）。
+#     # 用元组（Tuple）的形式返回在这一帧要被重新绘图的物体
+#     line.set_ydata(x - 5 + i)
+#     ax.set_xlabel(label)
+#     return line, ax
+#
+# if __name__ == '__main__':
+#     # FuncAnimation 会在每一帧都调用“update” 函数。
+#     # 在这里设置一个10帧的动画，每帧之间间隔200毫秒
+#     anim = FuncAnimation(fig, update, frames=np.arange(0, 10), interval=200)
+#     # if len(sys.argv) > 1 and sys.argv[1] == 'save':
+#     #     anim.save('line.gif', dpi=80, writer='imagemagick')
+#     # else:
+#     #     # plt.show() 会一直循环播放动画
+#     plt.show()
+
+
--- PycharmPro/spectro/Sound_DyImg.py
+++ PycharmPro/spectro/Sound_DyImg.py
@@ -1,5 +1,6 @@
+# coding=utf8
 import wave
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 import struct
@@ -8,11 +9,11 @@ import wave as we
 from scipy.fftpack import fft,ifft
 
 
-def fjc_record(OutFile="../res/fjc.wav", Seconds=5):
+def fjc_record(OutFile="../res/fjc.wav", Seconds=1):
     # 参数定义
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
-    CHANNELS = 2
+    CHANNELS = 1 # 通过play函数测试得出结论 channels=1 占用2个字节，channels=2占用4个字节
     RATE = 44100
 
     p = pyaudio.PyAudio()
@@ -24,12 +25,19 @@ def fjc_record(OutFile="../res/fjc.wav", Seconds=5):
 
     print("* recording")
 
+
+
     frames = []
     for i in range(0, int(RATE / CHUNK * Seconds)):
         data = stream.read(CHUNK)
-        # print(type(data))
-        frames.append(data)
-        intdata = int.from_bytes(data,byteorder='big',signed=False);
+        intdata = np.fromstring(data, dtype=np.short)
+        # if i==0:
+        #     print(type(data),data)
+            # print(data.split(b'|'))
+            # for idx in range(0,len(data),2):
+            #     chunk_one = data[idx:idx+2]
+            #     print("i:",idx,chunk_one,int.from_bytes(chunk_one,byteorder='big'))
+        frames.append(intdata)
         # print(intdata)
         spec = fft(intdata)
         print(np.array(spec).shape)
@@ -46,4 +54,78 @@ def fjc_record(OutFile="../res/fjc.wav", Seconds=5):
     wf.writeframes(b''.join(frames))
     wf.close()
 
+
 fjc_record()
+
+
+# 读取大小为1，查看输出内容，编码的宽度b'\x00\x00'
+def play():
+    # 读取wav文件，查看编码方式，只读1或几帧
+    wavfile = wave.open(r"./res/yusheng1.wav", "rb")
+    params = wavfile.getparams()
+    print("params:", params)
+    framesra, nframes = params[2], params[3]
+    # datawav = wavfile.readframes(nframes)
+    datawav = wavfile.readframes(1)
+    print("datawav:",type(datawav), datawav)
+    return
+    wavfile.close()
+    data = np.fromstring(datawav, dtype=np.short)
+    time = np.arange(0, nframes) * (1.0 / framesra)
+    plt.plot(time,data)
+    plt.show()
+    #############
+
+    # 用文本文件记录wave模块解码每一帧所产生的内容。注意这里不是保存为二进制文件
+    dump_buff_file = open(r"./res/fjc.dup", 'w')
+
+    chunk = 4  # 指定WAV文件的大小
+    wf = wave.open(r"./res/yusheng1.wav", 'rb')  # 打开WAV文件
+
+
+
+    p = pyaudio.PyAudio()  # 初始化PyAudio模块
+
+    # 打开一个数据流对象，解码而成的帧将直接通过它播放出来，我们就能听到声音啦
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()), channels=wf.getnchannels(),
+                    rate=wf.getframerate(), output=True)
+
+    data = wf.readframes(chunk)  # 读取第一帧数据
+    print(data)  # 以文本形式打印出第一帧数据，实际上是转义之后的十六进制字符串
+
+
+
+    # 播放音频，并使用while循环继续读取并播放后面的帧数
+    # 结束的标志为wave模块读到了空的帧
+    while data != b'':
+        stream.write(data)  # 将帧写入数据流对象中，以此播放之
+        data = wf.readframes(chunk)  # 继续读取后面的帧
+        dump_buff_file.write(str(data) + "\n---------------------------------------\n")  # 将读出的帧写入文件中，每一个帧用分割线隔开以便阅读
+
+    stream.stop_stream()  # 停止数据流
+    stream.close()  # 关闭数据流
+    p.terminate()  # 关闭 PyAudio
+    print('play函数结束！')
+
+
+# play()
+
+
+def TestBytesToInt():
+    num1 = int.from_bytes(b'12',byteorder='big')
+    num2 = int.from_bytes(b'12',byteorder='little')
+    print('%s,'%'num1',num1)
+    print('%s,' % 'num2', num2)
+
+    byt1 = (1024).to_bytes(2,byteorder='big')
+    byt2 = (1024).to_bytes(10,byteorder='big')
+    # byt3 = (-1024).to_bytes(10,byteorder='big')#can't convert negative int to unsigned
+    byt3=(3).to_bytes(10,byteorder='big')
+    lis1=['byt1','byt2','byt3']
+    lis2=[byt1,byt2,byt3]
+    lis3=zip(lis1,lis2)
+    dic=dict(lis3)
+    print(dic)
+    print(bytearray(b'\x04\x00')[0])
+
+# TestBytesToInt()
