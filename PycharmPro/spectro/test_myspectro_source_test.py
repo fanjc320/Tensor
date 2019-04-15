401cea61d619cb98cbcc0bbf9e342e362bf0126b
fanjinchang <894020798@qq.com>
2019-04-15 23:52:58 +0800

    dddd

--- PycharmPro/spectro/test_myspectro_source_test.py
+++ PycharmPro/spectro/test_myspectro_source_test.py
@@ -4,7 +4,7 @@ import matplotlib.cm as cm
 from matplotlib.path import Path
 import matplotlib.pyplot as plt
 # from skimage import data, filters
-import test_img_sharp
+# import test_img_sharp
 # import test_img_sharp1
 
 def mapTo(n, start1, stop1, start2, stop2):
--- PycharmPro/spectro/test_spec.py
+++ PycharmPro/spectro/test_spec.py
@@ -72,4 +72,4 @@ def GetWavData():
 
     return Z
 
-# GetWavData()
+GetWavData()
